56765d58100ca89a6498ed24ee7c1f5a8bbac4bf
kkamara <kamaracomputers@gmail.com>
2023-07-05 11:05:05 +0100

    Add asymmetric-encryption.js

---
+++ src/asymmetic-encryption.js
@@ -0,0 +1,17 @@
+// const {  publicEncrypt, privateDecrypt, } = require('crypto');
+// const { publicKey, privateKey, } = require('./keypair');
+
+// const secretMessage = Buffer.from("secret", 'utf8');
+// const encryptedData = publicEncrypt(
+//   publicKey,
+//   secretMessage,
+// );
+
+// console.log(encryptedData.toString('hex'))
+
+// const decryptedData = privateDecrypt(
+//     privateKey,
+//     encryptedData,
+// );
+
+// console.log(decryptedData.toString('utf-8'));
